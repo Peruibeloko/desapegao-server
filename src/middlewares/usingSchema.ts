db7a0aea87cda51d73dd32f132b0a65a42a830a6
peruibeloko <cmendonca32@gmail.com>
2024-04-05 17:43:42 -0300

    add more logging

--- src/middlewares/usingSchema.ts
+++ src/middlewares/usingSchema.ts
@@ -4,8 +4,13 @@ import { z } from 'zod';
 type ValidationMiddleware = <T extends z.ZodSchema>(schema: T) => (data: unknown, c: Context) => z.infer<typeof schema>;
 
 export const usingSchema: ValidationMiddleware = schema => (body, c) => {
+  console.debug('Parsing payload');
   const result = schema.safeParse(body);
-  if (!result.success) return c.text('Invalid listing', 400);
+  if (!result.success) {
+    console.debug('No bueno');
+    return c.text('Invalid listing', 400);
+  }
+  console.debug('All good!');
   const { data } = result;
   return data as z.infer<typeof schema>;
 };
