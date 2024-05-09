422506934d1a86ef5bc298afff4b8ff80693338b
peruibeloko <cmendonca32@gmail.com>
2024-05-09 14:56:30 -0300

    improve logging

--- src/middlewares/usingSchema.ts
+++ src/middlewares/usingSchema.ts
@@ -1,16 +1,22 @@
 import { Context } from 'hono/mod.ts';
+import { formatError } from '@/models/Listing.ts';
 import { z } from 'zod';
 
 type ValidationMiddleware = <T extends z.ZodSchema>(schema: T) => (data: unknown, c: Context) => z.infer<typeof schema>;
 
 export const usingSchema: ValidationMiddleware = schema => (body, c) => {
-  console.debug('Parsing payload');
+  const reqId = c.get('reqId');
+  console.log(`[${reqId}]`, 'Parsing payload');
+
   const result = schema.safeParse(body);
+
   if (!result.success) {
-    console.debug('No bueno');
-    return c.text('Invalid listing', 400);
+    console.log(`[${reqId}]`, 'Invalid payload', formatError(result.error));
+    return c.json({ reqId, ...formatError(result.error) }, 400);
   }
-  console.debug('All good!');
+
+  console.log(`[${reqId}]`, 'All good!');
+
   const { data } = result;
   return data as z.infer<typeof schema>;
 };
